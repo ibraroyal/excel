5293e19ee8cf4be148e3d2840c0f2c776dea0e69
lasvistass <spinelli1k90@gmail.com>
2022-01-18 10:49:37 +0100

    lombok ok

--- src/main/java/com/netgroup/exceldemo/data/dao/Excel.java
+++ src/main/java/com/netgroup/exceldemo/data/dao/Excel.java
@@ -42,49 +42,7 @@ public class Excel {
 	
 
 	
-	public LocalDate getLocaldate() {
-		return localdate;
-	}
-
-	public void setLocaldate(LocalDate localdate) {
-		this.localdate = localdate;
-	}
-
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNomeProdotto() {
-		return nomeProdotto;
-	}
-
-	public void setNomeProdotto(String nomeProdotto) {
-		this.nomeProdotto = nomeProdotto;
-	}
-
 	
-
-	public CategoriaProdotto getCategoriaProdotto() {
-		return categoriaProdotto;
-	}
-
-	public void setCategoriaProdotto(CategoriaProdotto categoriaProdotto) {
-		this.categoriaProdotto = categoriaProdotto;
-	}
-
-	public double getPrezzo() {
-		return prezzo;
-	}
-
-	public void setPrezzo(double prezzo) {
-		this.prezzo = prezzo;
-	}
-
 
 
 
